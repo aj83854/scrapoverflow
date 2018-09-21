d72b3731616a78c66aa834a969165979b93aad32
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-09-21 12:51:57 -0500

    Created constructor function defining Mongo settings pipeline

--- stackscraper/stackscraper/pipelines.py
+++ stackscraper/stackscraper/pipelines.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*-
+import pymongo
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.conf import settings
 
 
-class StackscraperPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class MongoDBPipeline(object):
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
