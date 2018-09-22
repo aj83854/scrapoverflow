9b88466cdbd9bb6fce86d42f7702400560b44b78
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-09-22 08:03:19 -0500

    Establish connection to DB, unpack data and save to DB

--- stackscraper/stackscraper/pipelines.py
+++ stackscraper/stackscraper/pipelines.py
@@ -1,9 +1,12 @@
 import pymongo
 
 from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+from scrapy import log
 
 
 class MongoDBPipeline(object):
+
     def __init__(self):
         connection = pymongo.MongoClient(
             settings['MONGODB_SERVER'],
@@ -11,3 +14,17 @@ class MongoDBPipeline(object):
         )
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem(f"Missing {data}!")
+        if valid:
+            self.collection.insert(dict(item))
+            log.msg(
+                "Question added to MongoDB database!",
+                level=log.DEBUG, spider=spider
+            )
+        return item
